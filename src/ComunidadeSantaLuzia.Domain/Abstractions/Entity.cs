9440aee6926ac3cd5ebdab4c6914e116a9ebe176
Regis Lima <regislima.105@gmail.com>
2024-08-31 15:21:52 -0300

    FEAT: Implementação inicial da classe Entity

--- src/ComunidadeSantaLuzia.Domain/Abstractions/Entity.cs
+++ src/ComunidadeSantaLuzia.Domain/Abstractions/Entity.cs
@@ -2,5 +2,8 @@ namespace ComunidadeSantaLuzia.Domain.Abstractions;
 
 public class Entity
 {
+    public Guid Id { get; init; }
 
+    protected Entity() { }
+    protected Entity(Guid id) => Id = id;
 }
