0b45aae34d52c4c09744a5699646d3003f5ac93a
Regis Lima <regislima.105@gmail.com>
2024-08-31 15:29:04 -0300

    Revert "FEAT: Implementação inicial da classe Entity"
    This reverts commit 9440aee6926ac3cd5ebdab4c6914e116a9ebe176.

--- src/ComunidadeSantaLuzia.Domain/Abstractions/Entity.cs
+++ src/ComunidadeSantaLuzia.Domain/Abstractions/Entity.cs
@@ -2,8 +2,5 @@ namespace ComunidadeSantaLuzia.Domain.Abstractions;
 
 public class Entity
 {
-    public Guid Id { get; init; }
 
-    protected Entity() { }
-    protected Entity(Guid id) => Id = id;
 }
